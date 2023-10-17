9dd1bf1ebfbe03fa2dad96aec6eb8d2047779a9f
arnaldochm <arnaldochacon1996@gmail.com>
2023-10-17 20:53:04 +0000

    Exercise Completed

--- exercises/12-post-request/app.py
+++ exercises/12-post-request/app.py
@@ -1,3 +1,10 @@
 import requests
 
 # your code here
+import requests
+url = 'https://assets.breatheco.de/apis/fake/sample/post.php'
+myobj = {'somekey': 'somevalue'}
+
+response = requests.post(url, json = myobj)
+
+print(response.text)
--- exercises/13-post-request-body/app.py
+++ exercises/13-post-request-body/app.py
@@ -1,4 +1,13 @@
 import requests
 
-resp = requests.post("https://assets.breatheco.de/apis/fake/sample/save-project-json.php")
+
+url = "https://assets.breatheco.de/apis/fake/sample/save-project-json.php"
+myobj = {
+    "id":2323,
+    "title": "Very big project"
+    }
+
+headers = {'Content-Type': 'application/json'}
+
+resp = requests.post(url, json=myobj, headers=headers)
 print(resp.text)
