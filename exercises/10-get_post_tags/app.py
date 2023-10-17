a725da21f427215cfee141ec43edde71cec6d6a8
arnaldochm <arnaldochacon1996@gmail.com>
2023-10-17 20:41:22 +0000

    WIP

--- exercises/10-get_post_tags/app.py
+++ exercises/10-get_post_tags/app.py
@@ -2,7 +2,16 @@ import requests
 
 def get_post_tags(post_id):
     # your code here
-    return None
+    tags = []
+
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+
+    json_response = response.json()
+
+    post_by_id = [post for post in json_response['posts'] if post.get('id') == post_id]
+
+    return post_by_id[0]['tags']
+    
 
 
-print(get_post_tags(146))
+print(get_post_tags(135))
--- exercises/11-get_attachment_by_id/app.py
+++ exercises/11-get_attachment_by_id/app.py
@@ -1,7 +1,16 @@
 import requests
 
 def get_attachment_by_id(attachment_id):
-    # your code here
+    
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+
+    json_response = response.json()
+
+    for post in json_response['posts']:
+        for atttachment in post['attachments']:
+            if atttachment.get('id') == attachment_id:
+                return atttachment.get('title')
+
     return None
 
-print(get_attachment_by_id(137))
+print(get_attachment_by_id(138))
